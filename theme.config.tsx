fe94d251d75676f7bdd57ac932f05ce9d0104e57
lincx <wktl1991504424@gmail.com>
2023-06-22 19:23:37 +0800

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,55 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>
+    Lincx
+    <style jsx>{`
+      span {
+        padding: 0.5rem 0.5rem 0.5rem 0;
+        font-size: 18px;
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+      }
+      span:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+    </span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Lincx'
+    }
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/lcx12901/whoBlog',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/lcx12901/whoBlog/tree/main/',
+  editLink: {
+    text: 'Edit this page on GitHub →'
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'feedback'
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: '学固不在乎读书，然不读书则义理无由明',
   },
 }
 
